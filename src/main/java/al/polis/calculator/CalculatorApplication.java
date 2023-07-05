5a928edbf2e6c8ea14e1f28f4702a997620dfd60
henryk001 <hkrosi70@icloud.com>
2023-07-05 18:41:50 +0200

    Update CalculatorApplication.java

--- src/main/java/al/polis/calculator/CalculatorApplication.java
+++ src/main/java/al/polis/calculator/CalculatorApplication.java
@@ -1,13 +1,1 @@
 package al.polis.calculator;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class CalculatorApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CalculatorApplication.class, args);
-	}
-
-}
